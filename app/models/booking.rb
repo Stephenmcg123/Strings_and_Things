efe22c95acf17c47d40f92e75f8284ebea96d1c0
itab ghazouani <itab.ghazouani@gmail.com>
2022-07-26 21:46:08 +0200

    Minues price issue fixed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,13 @@ class Booking < ApplicationRecord
 
   validates :start_date, presence: true
   validates :return_date, presence: true
+  validate :return_date_after_start_date
+
+  private
+
+  def return_date_after_start_date
+    return if return_date.blank? || start_date.blank?
+
+    errors.add(:return_date, "Must be after the pick up date") if return_date < start_date
+  end
 end
