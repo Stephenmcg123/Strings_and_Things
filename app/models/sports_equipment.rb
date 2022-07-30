1beb3bb864dc0815361861168c57070abbd0ed5f
Stephen McGowan <stephenmcg15@gmail.com>
2022-07-30 12:42:46 +0200

    removing geocode

--- app/models/sports_equipment.rb
+++ app/models/sports_equipment.rb
@@ -1,7 +1,6 @@
 class SportsEquipment < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  geocoded_by :address
 
   # after_validation :geocode, if: :will_save_change_address?
   include PgSearch::Model
